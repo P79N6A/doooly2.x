63701ca70819320426c43895f65858ab3111b6a7
yangwenwei <yangwenwei@97f0348b-149b-491d-833b-dba32c3ac2c0>
2018-06-19 02:06:00 +0000

    git-svn-id: svn://120.55.85.221/doooly/trunk/APP2.0/doooly@17468 97f0348b-149b-491d-833b-dba32c3ac2c0

--- src/main/java/com/doooly/business/common/service/ActivityCodeImageServiceI.java
+++ src/main/java/com/doooly/business/common/service/ActivityCodeImageServiceI.java
@@ -11,27 +11,27 @@ import com.doooly.entity.reachlife.LifeWechatBinding;
  * @version 1.0
  */
 public interface ActivityCodeImageServiceI {
-
+	//画布合并图片
 	public BufferedImage overlapImage(BufferedImage big, BufferedImage small,int height);
-	
+	//推送文字消息
 	public String sendText(String accessToken,String openId,JSONObject linkUrl,String sourceOpenId);
-
+	//推送图片
 	public String sendImage(String accessToken,String openId,String mediaId);
-
+	//推送活动消息
 	public String sendActivityText(String accessToken, String openId,String sourceOpenId);
-
-	public String sendActivityImage(String accessToken, String openId, String mediaId);
 	
+	public String sendActivityImage(String accessToken, String openId, String mediaId);
+	//生成微信带参二维码
 	public String getQRCodeUrl(String accessToken, String openId, String channel, String activityId);
 
 	public MessageDataBean pushImageAndText(String openId, String sourceOpenId, String channel, String activityId) throws Exception;
-	
+	//根据活动分别推送图文信息
 	public MessageDataBean pushNews(String openId, String channel, String activityKey) throws Exception;
 
 	public JSONObject getPhoneFee(String key);
-
+	//删除redis 的key
 	public void deleteTextInRedis();
-	
+	//获取微信昵称和头像
 	public LifeWechatBinding getWechatData(String openId);
 
 	String sendNews(String accessToken, String openId,List<JSONObject>articles);

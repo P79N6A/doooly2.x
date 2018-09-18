1c1e72ae6c34239a8529bacaa9ac3587dea29779
hutao <hutao@97f0348b-149b-491d-833b-dba32c3ac2c0>
2018-09-18 08:36:40 +0000

    git-svn-id: svn://120.55.85.221/doooly/trunk/APP2.0/doooly@18617 97f0348b-149b-491d-833b-dba32c3ac2c0

--- src/main/java/com/doooly/business/dict/impl/ConfigDictServiceImpl.java
+++ src/main/java/com/doooly/business/dict/impl/ConfigDictServiceImpl.java
@@ -9,20 +9,21 @@ import org.springframework.stereotype.Service;
 
 import com.doooly.business.dict.ConfigDictServiceI;
 import com.doooly.dao.reachad.AdConfigDictDao;
+
 @Service
 public class ConfigDictServiceImpl implements ConfigDictServiceI {
 	@Autowired
 	private StringRedisTemplate stringRedis;
 	@Autowired
 	private AdConfigDictDao dictDao;
-	
+
 	@Override
 	public String getValueByTypeAndKey(String dictType, String dictKey) {
 		String value = stringRedis.opsForValue().get(dictKey);
-		if(StringUtils.isBlank(value)){
-			value = dictDao.getValueByTypeAndKey(dictType, dictKey);
-			//设置超时时间，默认1天
-			stringRedis.opsForValue().set(dictKey, value.replaceAll("%s", "\n"), 1, TimeUnit.DAYS);
+		if (StringUtils.isBlank(value)) {
+			value = dictDao.getValueByTypeAndKey(dictType, dictKey).replaceAll("%s", "\n");
+			// 设置超时时间，默认1天
+			stringRedis.opsForValue().set(dictKey, value, 1, TimeUnit.DAYS);
 		}
 		return value;
 	}

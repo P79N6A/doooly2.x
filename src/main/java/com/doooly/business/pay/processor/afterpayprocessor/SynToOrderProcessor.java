cce96d60a2f3c44989afc292e360435af87fdf86
zhangqing <qing.zhang@reach-core.com>
2018-11-11 14:36:46 +0800

    订单同步

--- src/main/java/com/doooly/business/pay/processor/afterpayprocessor/SynToOrderProcessor.java
+++ src/main/java/com/doooly/business/pay/processor/afterpayprocessor/SynToOrderProcessor.java
@@ -110,7 +110,13 @@ public class SynToOrderProcessor implements AfterPayProcessor{
 					OrderDetail d = new OrderDetail();
 					d.setOrderid(o.getId().intValue());
 					d.setCode(itVo.getCode());
-					d.setGoods(itVo.getGoods() + itVo.getSku());
+                    String goods;
+                    if(itVo.getSku()!=null){
+                        goods = itVo.getGoods()+ itVo.getSku();
+                    }else {
+                        goods = itVo.getGoods();
+                    }
+                    d.setGoods(goods);
 					d.setAmount(itVo.getAmount());
 					d.setPrice(itVo.getPrice());
 					d.setNumber(itVo.getNumber());

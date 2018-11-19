83b414f1c8197f476710a87b463a7cd405713309
wanghai <wangh_2@sina.com>
2018-11-19 14:44:25 +0800

    返积分

--- src/main/java/com/doooly/business/pay/processor/refundprocessor/RefundSyncOrderProcessor.java
+++ src/main/java/com/doooly/business/pay/processor/refundprocessor/RefundSyncOrderProcessor.java
@@ -1,13 +1,11 @@
 package com.doooly.business.pay.processor.refundprocessor;
 
 import com.doooly.business.order.vo.OrderVo;
-import com.doooly.dao.reachad.AdAvailablePointsDao;
-import com.doooly.dao.reachad.AdReturnFlowDao;
-import com.doooly.dao.reachad.AdReturnPointsDao;
-import com.doooly.dao.reachad.OrderDao;
+import com.doooly.dao.reachad.*;
 import com.doooly.dto.common.PayMsg;
 import com.doooly.entity.reachad.AdReturnFlow;
 import com.doooly.entity.reachad.AdReturnPoints;
+import com.doooly.entity.reachad.AdReturnPointsLog;
 import com.doooly.entity.reachad.Order;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
@@ -37,7 +35,7 @@ public class RefundSyncOrderProcessor implements AfterRefundProcessor {
     @Autowired
     private AdReturnPointsDao adReturnPointsDao;
     @Autowired
-    private AdAvailablePointsDao adAvailablePointsDao;
+    private AdReturnPointsLogDao adReturnPointsLogDao;
 
     @Override
     public PayMsg process(OrderVo order, Order o) {
@@ -50,33 +48,58 @@ public class RefundSyncOrderProcessor implements AfterRefundProcessor {
         List<Order> list = orderDao.findList(o);
         if(CollectionUtils.isNotEmpty(list)){
             for (Order order2 : list) {
-                //插入ad_return_points
-                AdReturnPoints adReturnPoints = new AdReturnPoints();
-                adReturnPoints.setUserId(String.valueOf(order.getUserId()));
-                adReturnPoints.setOrderId(order2.getId());
-                AdReturnPoints adReturnPoints1 = adReturnPointsDao.get(adReturnPoints);
-                if(adReturnPoints1 != null){
-                    //说明已经同步过
-                    continue;
+                try {
+                    //插入ad_return_points_log
+                    AdReturnPointsLog adReturnPointsLog = new AdReturnPointsLog();
+                    adReturnPointsLog.setOrderId(order2.getId());
+                    adReturnPointsLog.setType(AdReturnPoints.TYPE_INTERCHANGE);
+                    AdReturnPointsLog adReturnPointsLog1 = adReturnPointsLogDao.getByCondition(adReturnPointsLog);
+                    if (adReturnPointsLog1 != null) {
+                        continue;
+                    }
+
+                    //插入ad_return_points
+                    AdReturnPoints adReturnPoints = new AdReturnPoints();
+                    adReturnPoints.setUserId(String.valueOf(order.getUserId()));
+                    adReturnPoints.setOrderId(order2.getId());
+                    AdReturnPoints adReturnPoints1 = adReturnPointsDao.get(adReturnPoints);
+                    if(adReturnPoints1 == null){
+                        //插入
+                        adReturnPoints.setReportId(String.valueOf(order.getId()));
+                        adReturnPoints.setAmount(order2.getUserRebate());
+                        adReturnPoints.setType(AdReturnPoints.TYPE_INTERCHANGE);
+                        adReturnPoints.setStatus(AdReturnPoints.STATUS_EXPECTED);
+                        adReturnPoints.setCreateDate(new Date());
+                        adReturnPointsDao.insert(adReturnPoints);
+                    }
+                    adReturnPoints1 = adReturnPointsDao.get(adReturnPoints);
+
+                    adReturnPointsLog.setAdReturnPointsId(Long.parseLong(adReturnPoints1.getId()));
+                    adReturnPointsLog.setOperateAmount(order2.getUserRebate());
+                    adReturnPointsLog.setOperateType("2");
+                    adReturnPointsLogDao.save(adReturnPointsLog);
+
+                    //更新adReturnPoints
+                    adReturnPoints1.setAmount(adReturnPoints1.getAmount().subtract(adReturnPointsLog.getOperateAmount()));
+                    adReturnPointsDao.update(adReturnPoints1);
+
+                    //计算返利
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("orderId",order2.getId());
+                    map.put("orderNumber",order2.getOrderNumber());
+                    map.put("bussinessId",order2.getBussinessId());
+                    orderDao.computeRefundRebateAndSyncOrder(map);
+
+                    //计算完返利,重新查询order，同步ad_return_flow表
+                    Order order3 = orderDao.get(String.valueOf(order2.getId()));
+                    AdReturnFlow adReturnFlow = adReturnFlowDao.getByOrderId(order.getId(), order3.getSerialNumber(), String.valueOf(order3.getPayType()));
+                    adReturnFlow.setUserRebate(order3.getUserRebate());
+                    adReturnFlow.setBusinessRebateAmount(order3.getBusinessRebate());
+                    adReturnFlow.setType(null);//不更新type值
+                    adReturnFlowDao.updateByPrimaryKeySelective(adReturnFlow);
+                } catch (Exception e) {
+                    logger.error("processor退款回调异常：{} ",order2.getOrderNumber(),e);
                 }
-                Map<String, Object> map = new HashMap<>();
-                map.put("orderId",order2.getId());
-                map.put("orderNumber",order2.getOrderNumber());
-                map.put("bussinessId",order2.getBussinessId());
-                orderDao.computeRefundRebateAndSyncOrder(map);
-                //计算完返利,重新查询order，同步ad_return_flow表
-                Order order3 = orderDao.get(String.valueOf(order2.getId()));
-                AdReturnFlow adReturnFlow = adReturnFlowDao.getByOrderId(order.getId(), order3.getSerialNumber(), String.valueOf(order3.getPayType()));
-                adReturnFlow.setUserRebate(order3.getUserRebate());
-                adReturnFlow.setBusinessRebateAmount(order3.getBusinessRebate());
-                adReturnFlow.setType(null);//不更新type值
-                adReturnFlowDao.updateByPrimaryKeySelective(adReturnFlow);
-                adReturnPoints.setReportId(String.valueOf(order.getId()));
-                adReturnPoints.setAmount(order3.getUserRebate());
-                adReturnPoints.setType(AdReturnPoints.TYPE_INTERCHANGE);
-                adReturnPoints.setStatus(AdReturnPoints.STATUS_EXPECTED);
-                adReturnPoints.setCreateDate(new Date());
-                adReturnPointsDao.insert(adReturnPoints);
             }
         }
         return null;

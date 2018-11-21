0a2727cf349459cdeab30e91b2b7ba06b5f36afe
wanghai <wangh_2@sina.com>
2018-11-21 13:23:44 +0800

    距离太大不用返回

--- src/main/java/com/doooly/business/mall/service/Impl/MallBusinessService.java
+++ src/main/java/com/doooly/business/mall/service/Impl/MallBusinessService.java
@@ -283,7 +283,10 @@ public class MallBusinessService implements MallBusinessServiceI {
 				if (a.getLatitude()!=null&&a.getLatitude()!=""&&a.getLongitude()!=null&&a.getLongitude()!="") {
 					double distance = ComputedRangeUtil.GetDistance(lat, lng, Double.valueOf(a.getLatitude()), Double.valueOf(a.getLongitude()));
 					a.setDistance(distance);
-					result.add(a);
+					//只返回50km以内的
+					if (distance <= 50) {
+						result.add(a);
+					}
 				}
 			}
 			Collections.sort(result,new Comparator<AdBusinessStore>() {

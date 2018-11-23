5bb73eaa35b8ceeb077e286c1c375917492068dd
huaxp <xianpeng.hua@reach-core.com>
2018-11-23 16:48:13 +0800

    我的订单修改

--- src/main/java/com/doooly/business/myorder/service/OrderService.java
+++ src/main/java/com/doooly/business/myorder/service/OrderService.java
@@ -13,19 +13,52 @@ import com.doooly.business.myorder.po.OrderPoReq;
 import com.doooly.business.myorder.po.OrderPoResp;
 
 public interface OrderService {
-
+	/**
+	 * 订单列表
+	 * @param req
+	 * @return
+	 */
 	public List<OrderPoResp> getOrderList(OrderReq req);
-	
+
+	/**
+	 * 订单详情
+	 * @param req
+	 * @return
+	 */
 	public OrderDetailResp getOrderDetail(OrderDetailReq req);
-	
+
+	/**
+	 * 统计订单分组
+	 * @param req
+	 * @return
+	 */
 	public List<Map<String,String>> getOrderdDetailSum(OrderPoReq req);
-	
+
+	/**
+	 * 统计订单总数
+	 * @param orderReq
+	 * @return
+	 */
 	public Long countOrderNum(OrderReq orderReq);
-	
+
+	/**
+	 * 取消订单提醒
+	 * @param req
+	 */
 	public void  cannelHint(OrderReq req);
-	
+
+	/**
+	 * 获取订单提醒
+	 * @param req
+	 * @return
+	 */
 	public HintResp getHint(HintReq req);
-	
+
+	/**
+	 * 删除订单
+	 * @param req
+	 * @return
+	 */
 	public boolean deleteOrder(OrderDeleteReq req);
 
 }

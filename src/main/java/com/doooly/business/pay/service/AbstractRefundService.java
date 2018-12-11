254ea30742b9c95451d56b61bbcef0c489cab736
zhangqing <qing.zhang@reach-core.com>
2018-12-11 15:47:58 +0800

    自动退款修改

--- src/main/java/com/doooly/business/pay/service/AbstractRefundService.java
+++ src/main/java/com/doooly/business/pay/service/AbstractRefundService.java
@@ -138,7 +138,7 @@ public abstract class AbstractRefundService implements RefundService {
                             Map<String, Object> map = (Map<String, Object>) resultModel.getData();
                             merchantRefundNo = (String) map.get("merchantRefundNo");
                             //查询待退款流水
-                            List<AdReturnFlow> listByOrderId = adReturnFlowDao.getListByOrderId(order.getId(), returnFlowNumber, null);
+                            List<AdReturnFlow> listByOrderId = adReturnFlowDao.getListByOrderId(order.getId(), null, null);
                             for (AdReturnFlow returnFlow : listByOrderId) {
                                 resultModel = dooolyPayRefund(order, merchantRefundNo, returnFlow.getType());
                             }

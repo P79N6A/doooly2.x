4d4c9f6c1f0a229cb77740c9d5551a6d0501d561
wanghai <wangh_2@sina.com>
2018-12-20 13:38:13 +0800

    详情

--- src/main/java/com/doooly/business/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/doooly/business/product/service/impl/ProductServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import com.doooly.dao.reachad.*;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -23,11 +24,6 @@ import com.doooly.business.product.entity.AdSelfProductSku;
 import com.doooly.business.product.entity.AdSelfProductType;
 import com.doooly.business.product.service.ProductService;
 import com.doooly.business.utils.Pagelab;
-import com.doooly.dao.reachad.AdGroupDao;
-import com.doooly.dao.reachad.AdOrderReportDao;
-import com.doooly.dao.reachad.AdSelfProductDao;
-import com.doooly.dao.reachad.AdSelfProductImageDao;
-import com.doooly.dao.reachad.AdUserDao;
 import com.doooly.dto.common.MessageDataBean;
 import com.doooly.entity.reachad.AdGroup;
 import com.doooly.entity.reachad.AdUser;
@@ -163,6 +159,9 @@ public class ProductServiceImpl implements ProductService {
 		return map;
 	}
 
+	@Autowired
+	private AdConfigDictDao adConfigDictDao;
+
 	@Override
 	public HashMap<String, Object> getSelfProductDetail(String productId, String userId,String activityName) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
@@ -204,6 +203,10 @@ public class ProductServiceImpl implements ProductService {
 			// 3.获取商品 id|商品名称|商品详情|商户名称|品牌介绍
 			AdSelfProduct adSelfProduct = adSelfProductDao.getSelfProductDetailById(productId);
 
+			String activityNameConfig = adConfigDictDao.getValueByTypeAndKey("AIRPORT_ACTIVITY_NAME","AIRPORT_ACTIVITY_NAME");
+			if (StringUtils.isNotEmpty(activityNameConfig) && activityNameConfig.contains(activityName)) {
+				activityName = "AirportActivity";
+			}
 			// 4.获取商品规格|最低销售价|市场价(除去被屏蔽的sku)
 			List<AdSelfProductSku> skuList = adSelfProductDao.getSelfProductSkuList(productId, limitSkuIdList,
 					group.getId(),activityName);

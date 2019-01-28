817c0a6965f3eac6ecf4398678384b72c3f13333
wanghai <wangh_2@sina.com>
2019-01-28 17:52:35 +0800

    channel

--- src/main/java/com/doooly/business/common/service/impl/AdUserService.java
+++ src/main/java/com/doooly/business/common/service/impl/AdUserService.java
@@ -1218,6 +1218,7 @@ public class AdUserService implements AdUserServiceI {
 		String staffNum = paramData.getString("staffNum");
 		String email = paramData.getString("email");
 		String groupId = paramData.getString("groupId");
+		String channel = paramData.getString(Constants.CHANNEL);
 		try {
 			Long startTime = System.currentTimeMillis();
 			if (code.length() == 6) {
@@ -1241,7 +1242,7 @@ public class AdUserService implements AdUserServiceI {
 								&& ConstantsLogin.CodeActive.SUCCESS.getCode().equals(resultData.getString("code"))) {
 							isFailed = false;
 							try {
-								resultData.put(Constants.CHANNEL, Constants.CHANNEL_H5);
+								resultData.put(Constants.CHANNEL, channel);
 								resultData = userService.userLogin(resultData);
 							} catch (Exception e) {
 								logger.info("verifyCodeAndActivation登录异常：", e);

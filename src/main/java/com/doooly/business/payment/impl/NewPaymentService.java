eaf7771756599db415000253f69c34feede82cbb
zhangqing <qing.zhang@reach-core.com>
2019-02-20 15:23:25 +0800

    修复支付宝退款插入流水_order表payType字段

--- src/main/java/com/doooly/business/payment/impl/NewPaymentService.java
+++ src/main/java/com/doooly/business/payment/impl/NewPaymentService.java
@@ -1368,7 +1368,6 @@ public class NewPaymentService implements NewPaymentServiceI {
                 int payType1 = Integer.parseInt(payType);
                 if (payType1 != 0 && refundFee != null && refundFee != null && (new BigDecimal(refundFee).compareTo(new BigDecimal("0")) > 0)) {
                     //非积分需要插入流水
-                    payType1 = 3;//微信
                     saveOneOrder(order, payType1, refundFee, refundFee, merchantRefundNo);
                 }
                 //积分退款要修改businessId一致

4e401e01e30a1e74c4669e242fd7963ba816c7a4
wanghai <wangh_2@sina.com>
2019-02-26 15:19:18 +0800

    美团 data

--- src/main/java/com/doooly/business/meituan/impl/MeituanServiceImpl.java
+++ src/main/java/com/doooly/business/meituan/impl/MeituanServiceImpl.java
@@ -164,7 +164,7 @@ public class MeituanServiceImpl implements MeituanService{
         }
 
         BigDecimal total = json.getBigDecimal("tradeAmount");
-        String orderNum = json.getString("sqtOrderId");
+        String orderNum = json.getString("serialNum") + "_" +json.getString("sqtOrderId");
         JSONObject param = new JSONObject();
         param.put("businessId", MeituanConstants.meituan_bussinesss_serial);//商户编号
         param.put("cardNumber", adUser.getTelephone());
